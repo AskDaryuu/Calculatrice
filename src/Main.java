0444609c9abd9eaa0b540e960e3effd331ed925f
Reda JAALI <redajaali@gmail.com>
2024-03-07 10:01:10 +0100

    ok

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // Path: src/Hello.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("test World");
     }
 }
