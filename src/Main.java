c01b2d4a72122725c67c74111fa9ebfcd3512022
Ryaaan78955 <ryan.cecon@ensitech.eu>
2024-03-14 09:43:48 +0100

    maj main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // Path: src/Hello.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HAHAH World");
+        System.out.println("coucou World");
     }
 }
